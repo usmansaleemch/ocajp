501044cee565b0febd179a14ae7dcf17eb6b5eb0
Usman Chodhary <usaleemch@hotmail.com>
2017-08-19 11:08:33 +1000

    Java Basics
    OCA Java 8 Practice

---
+++ src/home/practice/java/basics/JavaBasics.java
@@ -0,0 +1,10 @@
+// By Default package imported is java.lang
+package home.practice.java.basics;
+// Public class should have same name as file name
+public class JavaBasics {
+    // String... args
+    // static public void main
+    static public void main(String... args){
+        System.out.println("Test");
+    }
+}
