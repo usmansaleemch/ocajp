baa547e90d120c021c2763c683fc594255432080
Usman Chodhary <usaleemch@hotmail.com>
2017-08-19 22:08:08 +1000

    Java Data Types Initial Practice

---
+++ src/home/practice/java/datatypes/DataTypeDemo.java
@@ -0,0 +1,22 @@
+package home.practice.java.datatypes;
+
+/**
+ *  Chapter: Java Data Type
+ */
+public class DataTypeDemo {
+
+    public static void main(String... args) {
+        int num =0, num2 = 90;
+        int z = 20;
+        System.out.println((float)z);
+        int amount = 9_99;
+        String name="";
+
+        int Integer = 10;
+        //name = name + "Chair";
+        System.out.println(name);
+
+
+    }
+
+}
